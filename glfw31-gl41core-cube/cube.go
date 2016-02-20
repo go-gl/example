5fb062ab0713c0dcf8f7315c4b8df67f4883c2b6
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-20 00:08:00 -0800

    glfw31-gl41core-cube: Use gl.Strs.
    Use new gl.Strs from go-gl/gl#44.
    
    It's not strictly required for this minimal example to run in Go 1.6,
    but it's a good practice, and will serve as a canonical usage example.

--- glfw31-gl41core-cube/cube.go
+++ glfw31-gl41core-cube/cube.go
@@ -177,8 +177,9 @@ func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error)
 func compileShader(source string, shaderType uint32) (uint32, error) {
 	shader := gl.CreateShader(shaderType)
 
-	csource := gl.Str(source)
-	gl.ShaderSource(shader, 1, &csource, nil)
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
 	gl.CompileShader(shader)
 
 	var status int32
