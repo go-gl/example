ee0644b7c5650555db3c0f4d04f9ef5716e6c6ac
Dmitri Shuralyov <shurcooL@gmail.com>
2017-05-01 20:28:13 -0400

    Add import comments. (#61)
    The repository has been recently renamed from "examples" to "example"
    in #58. Help make the new expected import path more clear by adding
    import comments. (Reference: https://golang.org/cmd/go/#hdr-Import_path_checking.)
    
    This way, the expected import path is visible in the source code, in
    addition to README. It also gives a better error message when trying
    to go get or go install the package with incorrect old import path.
    
    Closes #58 (again).

--- gl41core-cube/cube.go
+++ gl41core-cube/cube.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Renders a textured spinning cube using GLFW 3 and OpenGL 4.1 core forward-compatible profile.
-package main
+package main // import "github.com/go-gl/example/gl41core-cube"
 
 import (
 	"fmt"
